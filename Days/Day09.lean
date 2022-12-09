d48cd51704940efa46f61aa78dee40fcb6da2d58
Jake Swenson <jake@jakeswenson.com>
2022-12-09 13:57:50 -0800

    day(9): part 2

--- Days/Day09.lean
+++ Days/Day09.lean
@@ -37,12 +37,65 @@ instance : Repr Point where
 
 structure Rope where
   head: Point
-  tail: Point
-  deriving Inhabited
+  tail: Array Point
+
+instance : Inhabited Rope where
+  default := { head := default, tail := #[default]}
 
 instance : Repr Rope where
   reprPrec rope _ := 
     .text s!"h{rope.head}{rope.tail}"
+
+def toArray (range: Std.Range) : Array Nat := Id.run <| do
+  let mut result := #[]
+
+  for i in range do
+    result := result.push i
+
+  return result
+
+def Rope.visualString (rope: Rope) : String := Id.run <| do
+  let points := rope.head::rope.tail.toList
+  let maxX: Int := points|>.map (·.x) |>.maximum? |>.get!
+  let minX: Int := points|>.map (·.x) |>.minimum? |>.get!
+  let maxY: Int := points|>.map (·.y) |>.maximum? |>.get!
+  let minY: Int := points|>.map (·.y) |>.minimum? |>.get!
+  if minY < 0 ∨ minX < 0 then panic! "Need to adjust min i guess"
+  
+  let gridSizeX := maxX + 1 |>.natAbs
+  let gridSizeY := maxY + 1 |>.natAbs
+
+  let squareGrid := Max.max gridSizeX gridSizeY
+
+  let mut grid: Array $ Array String := Array.mkArray squareGrid (Array.mkArray squareGrid "·")
+
+  for (idx, point) in points.enum do
+    let yRow: Array String := grid.get! point.y.natAbs
+    let char: String := 
+      if idx = 0 then "H" else 
+      if idx < 10 then toString idx
+      else "X"
+    let result := yRow.set! point.x.natAbs char
+    let newGrid := grid.set! point.y.natAbs result
+    grid := newGrid
+  
+  let p: Array String :=
+    grid.reverse 
+    |>.map Array.toList 
+    |>.map (String.intercalate "")
+
+  return p
+  |>.toList
+  |> (String.intercalate "\n")
+  |>.push '\n'
+  |>.append (Array.mkArray squareGrid "-" |>.toList |> String.intercalate "")
+  |>.push '\n'
+  |>.append ([:Min.min squareGrid 10] |> toArray |>.map toString |>.toList |> String.intercalate ""
+  |>.append s!"\n {squareGrid}×{squareGrid}")
+
+def Rope.print (rope: Rope) : IO Unit := do
+  IO.println rope.visualString
+  IO.println ""
 
 inductive Direction where
   | right : Direction 
@@ -63,39 +116,52 @@ def RopeMovement.parse : Parsec RopeMovement := do
   | 'L', cnt => return .move .left cnt.toNat!
   | m, _ => fail s!"Failed to parse rope movement from char '{m}'"
 
-
 def RopeMovement.parseMovements [ToInput α] (i: α) : Except String (Array RopeMovement) :=
   toInput i
   |>.lines
   |>.map (·.trim.text.iter)
   |>.map RopeMovement.parse
   |>.foldl (init:=.ok #[]) λ
-  | .error msg, _ => .error msg
-  | _, .error _ msg => .error msg
+  | .error msg, _ | _, .error _ msg => .error msg
   | .ok state, .success _ mvmt => .ok <| state.push mvmt
 
 #eval RopeMovement.parseMovements sample
 
-
-def adjustTail (oldHead: Point) (rope: Rope): Rope :=
-  match rope.tail, rope.head with
+def pointΔ (leader: Point) (follower: Point) : Point :=
+  match follower, leader with
   | { x:=tx, y:=ty }, { x:=hx, y:=hy } => 
-    let taxiDistance := (hx-tx).natAbs + (hy-ty).natAbs
-    if taxiDistance ≥ (if hx = tx ∨ hy = ty then 2 else 3) 
-    then {rope with tail := oldHead}
-    else rope
-    -- let Δx := ((hx-tx) + if hy ≠ ty then ((hy-ty)/2).natAbs else 0) /2
-    -- let Δy := ((hy-ty) + if hx ≠ tx then ((hx-tx)/2).natAbs else 0) /2
-    -- { rope with tail := { x := rope.tail.x + Δx, y := rope.tail.y + Δy } }
+    let Δx := hx-tx 
+    let Δy := hy-ty
+    { x:= Δx, y:=Δy }
 
--- T:0,0 H:1,2 → T:1,1 : tx+(hx-tx)/2, ty+(hx-tx)/2
--- T:0,0 H:2,0 → T:1,0 : tx+(hx-tx)/2,hx
--- 
+instance : Sub Point where
+  sub := pointΔ
+
+def Point.asUnitVec (point: Point) : Point :=
+  let {x, y} := point
+  { x:= if x = 0 then 0 else x/x.natAbs,
+    y:= if y = 0 then 0 else y/y.natAbs }
+
+instance : Add Point where
+  add p₁ p₂ := { x := p₁.x + p₂.x, y := p₁.y + p₂.y }
+
+def adjustKnot (leader: Point) (follower: Point) : Point :=
+  let delta@{ x:= Δx, y:=Δy } := leader - follower 
+  if Δx.natAbs ≥ 2 ∨ Δy.natAbs ≥ 2 
+  then follower + delta.asUnitVec
+  else follower
+
+def adjustTailKnots (rope: Rope): Rope :=
+  { rope with tail := foldAdjustKnots rope.head rope.tail }
+  where 
+    foldAdjustKnots (head: Point) (knots: Array Point): Array Point :=
+      knots.foldl (init:=(head, #[])) (λ
+      | (leader, tail), knot =>
+        let newPos := adjustKnot leader knot
+        (newPos, tail.push newPos))
+      |>.snd
+
 /-
-t(4,3)h(2,4) → T:3,4
-Δx = ((2-4)+(4-3)/2)/2 = (-2 + 0) / 2 = -1
-Δy = ((4-3)+(2-4)/2)/2 = 1 + -1 / 2
-
 ...H.. (3,4)
 ....T. (4,3)
 ......
@@ -108,7 +174,12 @@ s.....
 ......
 s.....
 -/
-#eval adjustTail (.mk 3 4) { head := .mk 2 4, tail := .mk 4 3 }
+#eval Rope.mk (.mk 3 4) #[.mk 4 3] |> Rope.print
+
+#eval adjustKnot (.mk 2 4) (.mk 4 3)
+#eval adjustTailKnots { head := .mk 2 4, tail := #[.mk 4 3] }
+
+#eval Rope.print <| adjustTailKnots { head := .mk 2 4, tail := #[.mk 4 3] }
 
 /-
 ..T...(2,4)
@@ -129,8 +200,8 @@ s.....
 ......
 s.....
 -/
-#eval adjustTail (.mk 2 3) { head := .mk 3 3, tail := .mk 2 4 }
-#eval adjustTail (.mk 4 3) { head := .mk 5 3, tail := .mk 3 4 }
+#eval adjustTailKnots { head := .mk 3 3, tail := #[.mk 2 4] }
+#eval adjustTailKnots { head := .mk 5 3, tail := #[.mk 3 4] }
 
 def Rope.move (rope: Rope) (mvmts: Array RopeMovement) : Array Rope :=
   rope
@@ -148,18 +219,26 @@ def Rope.move (rope: Rope) (mvmts: Array RopeMovement) : Array Rope :=
     | .move _ 0 => (rope, result)
     | .move dir (cnt + 1) => 
       let newRopeHead := mvHead dir rope
-      let newRope := adjustTail rope.head newRopeHead
+      let newRope := adjustTailKnots newRopeHead
       apply newRope (result.push newRope) (.move dir cnt) 
     
     applyMovements (rope: Rope) : Array Rope :=
       mvmts.foldl (init:=(rope, #[rope])) (λ
         | (rope, list), mvmt => apply rope list mvmt) |>.snd
 
+def printRopes (ropes: Array Rope) : IO Unit := do
+  for rope in ropes do
+    rope.print
+  return ()
+
 #eval Rope.move (default) <| (RopeMovement.parseMovements sample).toOption.get!
+
+#eval printRopes <| Rope.move (default) <| (RopeMovement.parseMovements sample).toOption.get!
 
 def Rope.tailPath (rope: Rope) (mvmts: Array RopeMovement) : Array Point :=
   rope.move mvmts
   |>.map (·.tail)
+  |>.map (λ knots => knots.reverse.toList.head!)
 
 /-
 ..##..
@@ -190,11 +269,33 @@ def part₁ (input: Input) : Nat :=
 /--
 -/
 def part₂ (input: Input) : Nat :=
-  0
+  mvmts
+  |> Rope.distinctTailPath (rope:=.mk default (Array.mkArray 9 default))
+  |>.size
+  where
+    mvmts: Array RopeMovement := 
+      match RopeMovement.parseMovements input with
+      | .ok res => res 
+      | .error error => panic! s!"Error: {error}"
 
 def solution : Problem Nat := ⟨ day, part₁, part₂ ⟩ 
 
-
 #eval testPart₁ solution sample (expect:=13)
-#eval testPart₂ solution sample (expect:=0)
+
+
+-- #eval printRopes <| Rope.move (rope:=.mk default (Array.mkArray 9 default)) <| (RopeMovement.parseMovements sample).toOption.get!
+#eval testPart₂ solution sample (expect:=1)
+
+def sample₂ := "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
+
+#eval printRopes <| Rope.move (rope:=.mk default (Array.mkArray 9 default)) <| (RopeMovement.parseMovements sample₂).toOption.get!
+#eval testPart₂ solution sample₂ (expect:=36)
+
 
